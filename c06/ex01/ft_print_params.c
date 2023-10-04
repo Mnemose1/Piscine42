d026f6163ffcda5b4226c23cca2796eed823c84a
Laura Malafaia <lauramalafaia1@gmail.com>
2023-10-04 19:15:13 +0100

    Exercicio c06

---
+++ c06/ex01/ft_print_params.c
@@ -0,0 +1,24 @@
+:Stdheard
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+int	main(int argc, char *argv[])
+{
+     if (argc >= 2){
+	    int	i;
+
+	    i = 0;
+	    while (argv[1][i] != '\0' && argc)
+	    {
+		    ft_putchar(argv[1][i]);
+		    i++;
+	    }
+    }	
+    ft_putchar('\n');
+    return (0);
+}
