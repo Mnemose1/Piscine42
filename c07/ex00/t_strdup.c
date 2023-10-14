9e66e3d19bb5095481ac1cc673592ec583a096cf
Laura Malafaia <lauramalafaia1@gmail.com>
2023-10-14 18:51:39 +0100

    Exercicio

---
+++ c07/ex00/t_strdup.c
@@ -0,0 +1,50 @@
+:Stdheader
+
+#include <stdlib.h>
+
+int	ft_str_length(char *str)
+{
+	int	index;
+
+	index = 0;
+	while (str[index])
+		index++;
+	return (index);
+}
+
+char	*ft_strdup(char *src)
+{
+	int		index;
+	char	*dest;
+	char	*d;
+
+	index = 0;
+	d = ((dest = (char *)malloc(ft_str_length(src) * sizeof(char) + 1)));
+	if (!d)
+	{
+		return (0);
+	}
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	dest[index] = '\0';
+	return (dest);
+}
+
+/*#include <string.h>
+#include <stdio.h>
+
+int		main(void)
+{
+	char	*str;
+	char	*allocated;
+
+	str = "Ola malloc()";
+	printf("original  : base  : $%s$ @ %p\n", str, str);
+	allocated = strdup(str);
+	printf("copied    : alloc : $%s$ @ %p\n", allocated, allocated);
+	allocated = ft_strdup(str);
+	printf("ft_copied : alloc : $%s$ @ %p\n", allocated, allocated);
+}*/
